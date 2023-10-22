606233b06a7b2c2bce7f42d7521f52def27e5f75
kebirna <kebirnab@gmail.com>
2023-10-22 07:41:11 -0700

    creat pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import Profile from "./pages/profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
-function App() {
+export default function App() {
   return (
-    <h1 className='text-red-500'  >
-      fifa
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
